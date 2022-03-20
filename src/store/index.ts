5b39f6037638a7b17372e45a95eaae8d437d3f1c
lyq <953342912@qq.com>
2022-03-20 18:41:28 +0800

    chore: mobx的proxy选项

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,8 @@ import { configure } from 'mobx';
 import { counterStore } from './counter';
 import { pageDataStore } from './pageData';
 
+// https://zh.mobx.js.org/configuration.html
+// 但是如过你的目标环境不支持 Proxy，你也可以通过配置将 Proxy 支持关闭: useProxies: 'never'
 configure({ enforceActions: 'always' });
 
 export const stores = { counterStore, pageDataStore };
