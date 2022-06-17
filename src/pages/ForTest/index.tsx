2fba430dba6bbe7c3f1e926568700dbdbb94d974
yongqing_luo <953342912@qq.com>
2022-06-17 13:40:44 +0800

    update

--- src/pages/ForTest/index.tsx
+++ src/pages/ForTest/index.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 import { QRCodeSVG } from 'qrcode.react';
+import MenuTabs from '@src/components/MenuTabs';
 
 const ForTest: FC = () => {
   return (
-    <div style={{ width: '500px', height: '500px' }}>
-      <QRCodeSVG value='https://reactjs.org/' size={200} style={{ margin: '0 auto' }} />
-    </div>
+    // <div style={{ width: '500px', height: '500px' }}>
+    //   <QRCodeSVG value='https://reactjs.org/' size={200} style={{ margin: '0 auto' }} />
+    // </div>
+    <MenuTabs />
   );
 };
 
