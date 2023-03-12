f672b762288b70e2d67c2d37a9d80ff0636fafc5
lyq <953342912@qq.com>
2023-03-12 23:07:43 +0800

    update

--- src/pages/ForTest/index.tsx
+++ src/pages/ForTest/index.tsx
@@ -1,12 +1,11 @@
 import { FC } from 'react';
+import styled, { keyframes } from 'styled-components';
 import Download from '@src/components/Download';
 
+const Component = styled('div')``;
+
 const ForTest: FC = () => {
-  return (
-    <div>
-      <Download />
-    </div>
-  );
+  return <Component as={Download} text='1111' />;
 };
 
 export default ForTest;
